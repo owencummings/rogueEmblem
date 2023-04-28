3c9fd44099244d5a318051dc31eafcc56eb51cc6
Owen Cummings <owenpatrickcummings@gmail.com>
2023-04-28 16:51:09 -0400

    Some functionality to weather manager

--- Assets/Scripts/Visual/WeatherManager.cs
+++ Assets/Scripts/Visual/WeatherManager.cs
@@ -7,17 +7,20 @@ public class WeatherManager : MonoBehaviour
     public Light lightSource;
     public Color closeAtmosphereColor = new Color(0.637f, 0.877f, 0.950f, 1f);
     public Color farAtmosphereColor = new Color(0.419f, 0.705f, 0.910f, 1f);
-    private Quaternion lightAngle;
 
     void Awake()
     {
-        RenderSettings.ambientLight = closeAtmosphereColor; // Currently I think the shader overshoots the lerp, so farAtmosphere is actually the midrange. 
+        RenderSettings.ambientLight = closeAtmosphereColor; // Currently I think the shader overshoots the lerp, so farAtmosphere is actually the midrange.
+        RenderSettings.ambientIntensity = 0f;
         RenderSettings.fog = true;
         RenderSettings.fogMode = FogMode.Linear;
         RenderSettings.fogColor = farAtmosphereColor;
         RenderSettings.fogDensity = 0.02f;
-        lightAngle = lightSource.transform.rotation;
     }
 
-
+    // TODO: add plane underneath to block light near nighttime
+    void FixedUpdate()
+    {
+        lightSource.transform.rotation *= Quaternion.AngleAxis(0.1f, Vector3.right);
+    }
 }
