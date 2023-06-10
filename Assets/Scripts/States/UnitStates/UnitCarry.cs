32db788f3ffbe7849db2b891ae6d8681fc2d5aa3
Owen Cummings <owenpatrickcummings@gmail.com>
2023-06-10 11:31:03 -0400

    Add position/angle orchestration for carrying units

--- Assets/Scripts/States/UnitStates/UnitCarry.cs
+++ Assets/Scripts/States/UnitStates/UnitCarry.cs
@@ -15,6 +15,7 @@ public class UnitCarry : IState
     private Transform _tf;
     private CarryData _carryData;
     public ICarryable carryable;
+    public int walkableMask = LayerMask.GetMask("Walkable");
 
     public UnitCarry(NavMeshAgent navMeshAgent, Rigidbody rb, Transform tf, CarryData carryData)
     {
@@ -26,12 +27,18 @@ public class UnitCarry : IState
 
     public void Tick()
     {
-        // Move towards carry pivot 
+        // Change position to match the ground
+        Vector3 targetPosition = _tf.position;
+        Vector3 origin = _carryData.carryTarget.transform.position + _carryData.carryPivot + 0.5f * Vector3.up;
+        RaycastHit hit;
+        if (Physics.Raycast(origin, Vector3.down, out hit, 1f, walkableMask)){
+            targetPosition = hit.point; // probably needs an offset
+        } 
         _tf.position = Vector3.MoveTowards(_tf.position,
-                                           _carryData.carryTarget.transform.position + _carryData.carryPivot + carryable.CurrCarryOffset * Vector3.down, 
+                                           targetPosition, 
                                            0.01f);
-        Vector3 toRotate =  _carryData.carryTarget.transform.position - _tf.position;
-        toRotate = new Vector3(toRotate.x, 0, toRotate.z);
+
+        Vector3 toRotate =  _carryData.carryTarget.transform.position + ((carryable.CurrCarryOffset + 0.5f) * Vector3.down) - _tf.position;
         _tf.rotation = Quaternion.LookRotation(Vector3.RotateTowards(_tf.forward, toRotate, 0.1f, 0.1f));
         // Assess if should continue carrying
     }
